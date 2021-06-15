48fe9dfb9ca0d358bc834aa6d8c40919d054636f
Alex93pa <Ale.pug2@gmail.com>
2021-06-15 16:04:32 +0200

    fine

--- index.php
+++ index.php
@@ -3,8 +3,24 @@
 class Movie {
     public $id;
     public $title;
-    public $genre;
     public $duration;
 
-    
+    function __construct($id, $title, $duration) {
+        $this->id = $id;
+        $this->title = $title;
+        $this->duration = $duration;
+
+    }
+
+    public function renderMovie() {
+        return $this->id . " - " . $this->title ." - " . $this->duration . "<br>";
+    }
 }
+
+
+$movie1 = new Movie("01", "che bella giornata", "92");
+
+$movie2 = new Movie("02", "sole a catinelle", "90");
+
+echo($movie1-> renderMovie());
+echo($movie2-> renderMovie());
